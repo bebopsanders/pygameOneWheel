83fff564a997f856cfb7f94106c44ec83e24871f
Tyson Van Buren <bebopsanders@gmail.com>
2021-03-07 11:16:27 -0700

    created update() method in Player, gave default acc (0,0), created an object keys from the function get_pressed() that checks for keyup&down, assigned movement keys a,s,d,w and added equations of motion

--- sprites.py
+++ sprites.py
@@ -8,6 +8,24 @@ class Player(pg.sprite.Sprite):
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH/2,HEIGHT/2)
+        #position
         self.pos = vec(WIDTH/2,HEIGHT/2)
+        #velocity
         self.vel = vec(0,0)
+        #acceleration
         self.acc = vec(0,0)
+    def update(self):
+        self.acc = vec(0,0)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.acc.x = -0.8
+        elif keys[pg.K_d]:
+            self.acc.x = 0.8
+        elif keys[pg.K_s]:
+            self.acc.y = 0.8
+        elif keys[pg.K_w]:
+            self.acc.y = -0.8
+        self.acc += self.vel * -0.12
+        self.vel += self.acc
+        self.pos += self.acc + 0.5 * self.vel
+        self.rect.center = self.pos
